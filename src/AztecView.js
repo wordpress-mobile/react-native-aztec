2bd3916718dadacca719820c60b39d3bc2d501e2
Elijah Windsor <ewindsor@gmail.com>
2022-01-14 14:50:47 -1000

    feat: when editor is pressed, call sendSpaceAndBackspace on Android

--- src/AztecView.js
+++ src/AztecView.js
@@ -196,6 +196,12 @@ class AztecView extends React.Component {
   _onPress = (event) => {
     this.focus(event); // Call to move the focus in RN way (TextInputState)
     this._onFocus(event); // Check if there are listeners set on the focus event
+
+    if (Platform.OS === 'android') {
+      setTimeout(() => {
+        this.sendSpaceAndBackspace(); 
+      }, 250);
+    }
   }
 
   render() {
